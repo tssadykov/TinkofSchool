f7b360ba0f7b505841b345ab09a48d7f8005693d
Тимур <tssadykov16@gmail.com>
2018-09-21 19:43:43 +0300

    App LifeCycle

--- TinkoffChat/AppDelegate.swift
+++ TinkoffChat/AppDelegate.swift
@@ -13,32 +13,48 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    var stringOfPreviousState: String = "not running"
+    
+    func printStateLog(_ functionName: String, from firstStatement: String, to secondStatement: String, didMoved: Bool){
+        #if DEBUG
+        if didMoved {
+            print("Application moved from \(firstStatement) to \(secondStatement): \(functionName)")
+        } else {
+            print("Application will move from \(firstStatement) to \(secondStatement): \(functionName)")
+        }
+        stringOfPreviousState = secondStatement
+        #endif
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        printStateLog(#function, from: stringOfPreviousState, to: "\(UIApplication.shared.applicationState)", didMoved: true)
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        
+        printStateLog(#function, from: "\(UIApplication.shared.applicationState)", to: "inactive", didMoved: false)
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
+        printStateLog(#function, from: stringOfPreviousState, to: "\(UIApplication.shared.applicationState)", didMoved: true)
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        
+        printStateLog(#function, from: "\(UIApplication.shared.applicationState)", to: "inactive", didMoved: false)
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
+        printStateLog(#function, from: stringOfPreviousState, to: "\(UIApplication.shared.applicationState)", didMoved: true)
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        
+        printStateLog(#function, from: "\(UIApplication.shared.applicationState)", to: "not running", didMoved: false)
     }
 
 
---
+++ TinkoffChat/UIApplicationState extensions/CustomStringConvertible.swift
@@ -0,0 +1,23 @@
+//
+//  UIApplicationState extensions.swift
+//  TinkoffChat
+//
+//  Created by Тимур on 21.09.2018.
+//  Copyright © 2018 Тимур. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIApplicationState: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .active:
+            return("active")
+        case .inactive:
+            return("inactive")
+        case .background:
+            return("background")
+        }
+    }
+}
